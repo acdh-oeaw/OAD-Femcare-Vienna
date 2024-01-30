31a363361dbfacd3e375bf024c20743c3fc5da4d
oliviareichl <Olivia.Reichl@oeaw.ac.at>
2024-01-30 16:27:25 +0100

    chore: defaultLocale is de

--- config/i18n.config.ts
+++ config/i18n.config.ts
@@ -1,24 +1,18 @@
+export const locales = ["de"];
+export type Locale = (typeof locales)[number];
 import type { LocaleObject } from "vue-i18n-routing";
 
 import type de from "@/messages/de/common.json";
 import type projectDe from "@/messages/de/project.json";
-import type en from "@/messages/en/common.json";
-import type projectEn from "@/messages/en/project.json";
 
 import { project } from "../config/project.config";
-
-export const locales = ["de", "en"] as const;
-
-export type Locale = (typeof locales)[number];
-
 export const defaultLocale: Locale = project.defaultLocale;
 
 export const localesMap = {
 	de: { code: "de", iso: "de", files: ["de/common.json", "de/project.json", "de/crm.json"] },
-	en: { code: "en", iso: "en", files: ["en/common.json", "en/project.json", "en/crm.json"] },
 } satisfies Record<Locale, LocaleObject>;
 
-export type Messages = typeof en & typeof projectEn;
+export type Messages = typeof de & typeof projectDe;
 
 export interface Schema {
 	message: Messages;
@@ -30,5 +24,4 @@ export function isValidLocale(value: string): value is Locale {
 
 export interface Translations extends Record<Locale, Messages> {
 	de: typeof de & typeof projectDe;
-	en: typeof en & typeof projectEn;
 }
